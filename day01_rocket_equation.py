00c256d166c829872b1569faccc8a10de6d6e97b
jenspfeifle <jens@pfeifle.tech>
2019-12-01 09:11:01 -0500

    day01, formatting

--- day01_rocket_equation.py
+++ day01_rocket_equation.py
@@ -1,9 +1,17 @@
-def read(filename = "day01.inp"):
-    with open(filename, "r") as f:
-        input = f.read().strip().split("\n")
+"""
+Day 1
+What is the sum of the fuel requirements for all of the modules on your spacecraft?
+(when also taking into account the mass of the added fuel?)
+"""
 
-    input = [int(i) for i in input]
-    return input
+
+def read(filename="day01.inp"):
+    """ Read and clean up input file """
+    with open(filename, "r") as f:
+        inpt = f.read().strip().split("\n")
+
+    return [int(i) for i in inpt]
+
 
 def fuel_required(module_mass: int) -> int:
     """ Calculate the fuel required to launch a module with a given mass"""
@@ -11,29 +19,34 @@ def fuel_required(module_mass: int) -> int:
     # divide by three
     # round down
     # and subtract 2.
-    fuel_req = max(0,int(module_mass)//3-2)
+    fuel_req = max(0, int(module_mass) // 3 - 2)
     # account for fuel required to launch the required fuel
     if fuel_req > 0:
         fuel_req += fuel_required(fuel_req)
     return fuel_req
 
+
 def tests():
+    """ run tests, AssertionError on fail """
     # tests
-    assert(fuel_required(12) == 2)
-    assert(fuel_required(14) == 2)
-    assert(fuel_required(1969) == 966)
-    assert(fuel_required(100756) == 50346)
+    assert fuel_required(12) == 2
+    assert fuel_required(14) == 2
+    assert fuel_required(1969) == 966
+    assert fuel_required(100756) == 50346
     print("tests passed.")
 
-def solve(input):
+
+def solve(inpt):
+    """ solve the puzzle and print the result"""
     total_fuel_requirement = 0
     # individually calculate the fuel needed for the mass of each module
     # then add together all the fuel values
-    for module_mass in input:
+    for module_mass in inpt:
         total_fuel_requirement += fuel_required(module_mass)
     print(f"total_fuel_requirement = {total_fuel_requirement}")
-    
+
+
 if __name__ == "__main__":
-    inpt = read()
+    INPUT = read()
     tests()
-    solve(inpt)
+    solve(INPUT)
